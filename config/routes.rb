ae51586fd9003219744b1778973be05f85ae94fd
nfukushim-cj <nfukushima.cj@gmail.com>
2024-05-12 13:46:53 +0000

    [start]ファイルの記述・CRUDの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "homes#top"
+  devise_for :users
+  get 'home/about' => 'homes#about'
+  resources :books, only:[:new, :index, :show, :destroy]
+  resources :users, only:[:edit, :index, :show]
 end
